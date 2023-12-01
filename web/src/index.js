5ff600504234dcc0b4c84f5125d3bb88ed9daeff
3dragons <3dragons@dragons.com>
2023-12-01 09:53:57 +0800

    add chest choose

--- web/src/index.js
+++ web/src/index.js
@@ -24,6 +24,78 @@ function changeDifficulty(index) {
     theDifficulty = index;
 }
 
+function changeChestFD(string, index) {
+    if(deChest.indexOf(de_chest) === index){
+        de_chest = null;
+        const button = deChestButton[index];
+        button.style.backgroundColor = '#00FFFF00';
+        button.style.color = '#00FF00'
+        return;
+    }
+    for (var i = 0; i < deChestButton.length; i++) {
+        const button = deChestButton[i];
+        if (i === index) {
+            button.style.backgroundColor = '#00FFFF80';
+            button.style.color = '#FFFFFF'
+            continue;
+        }
+        button.style.backgroundColor = '#00FFFF00';
+    }
+    de_chest = string;
+}
+
+function changeChestFB(string, index) {
+    if(bbChest.indexOf(bb_chest) === index){
+        bb_chest = null;
+        const button = bbChestButton[index];
+        button.style.backgroundColor = '#00FFFF00';
+        button.style.color = '#00FF00'
+        return;
+    }
+    for (var i = 0; i < bbChestButton.length; i++) {
+        const button = bbChestButton[i];
+        if (i === index) {
+            button.style.backgroundColor = '#00FFFF80';
+            button.style.color = '#FFFFFF'
+            continue;
+        }
+        button.style.backgroundColor = '#00FFFF00';
+    }
+    bb_chest = string;
+}
+
+function chestMouseoverFD(index) {
+    if(de_chest === deChest[index]){
+        return;
+    }
+    const button = deChestButton[index];
+    button.style.color = '#00FF00'
+}
+
+function chestMouseoutFD(index) {
+    if(de_chest === deChest[index]){
+        return;
+    }
+    const button = deChestButton[index];
+    button.style.color = '#FFFFFF'
+}
+
+function chestMouseoverFB(index) {
+    if(bb_chest === bbChest[index]){
+        return;
+    }
+    const button = bbChestButton[index];
+    button.style.color = '#00FF00'
+}
+
+function chestMouseoutFB(index) {
+    if(bb_chest === bbChest[index]){
+        return;
+    }
+    const button = bbChestButton[index];
+    button.style.color = '#FFFFFF'
+}
+
 function onMouseover(string, index) {
     if (string === 'map') {
         if (index === mapNameList.indexOf(map)) {
